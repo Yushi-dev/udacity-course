f9a96b1a7c7b4d34c0fa52accaa14c687f79280f
yushi <yushi.dev.hotspring@gmail.com>
2022-04-19 11:46:48 +0900

    2022/4/19 Success screen transition
    Possible transition from UniteList

--- course/04_navigation/task_04_navigation/lib/converter_route.dart
+++ course/04_navigation/task_04_navigation/lib/converter_route.dart
@@ -12,6 +12,7 @@ import 'package:task_04_navigation/unit.dart';
 ///
 /// While it is named ConverterRoute, a more apt name would be ConverterScreen,
 /// because it is responsible for the UI at the route's destination.
+
 class ConverterRoute extends StatelessWidget {
   /// Color for this [Category].
   final Color color;
@@ -27,28 +28,56 @@ class ConverterRoute extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  /// Test Screen transition
+  void _navigateToConverter(BuildContext context) {
+    String name = 'Success';
+    MaterialColor color = Colors.cyan;
+
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(
+              name,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            color: color,
+            units: units,
+          ),
+        );
+      },
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     final unitWidgets = units.map((Unit unit) {
       // TODO: Set the color for this Container
-      return Container(
-        color: color,
-        margin: const EdgeInsets.all(8.0),
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Text(
-              unit.name!,
-              style: Theme.of(context).textTheme.headline5,
+      return GestureDetector(
+          onTap: () => _navigateToConverter(context),
+          child: Container(
+            color: color,
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  unit.name!,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Text(
+                  'Conversion: ${unit.conversion}',
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+              ],
             ),
-            Text(
-              'Conversion: ${unit.conversion}',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          ],
-        ),
-      );
+          ));
     }).toList();
 
     return ListView(
